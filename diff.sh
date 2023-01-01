c271067baa7dc683f4b7b5b4e5e592de688e3102
topcue <topcue1019@naver.com>
2023-01-01 16:57:42 +0900

    Init

---
+++ diff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BASE_PATH=$HOME/comprov
+PATH_A=$BASE_PATH/$1
+PATH_B=$BASE_PATH/$2
+
+if [ $# -eq 2 ]; then
+  for file in `find $PATH_A/ -type f -printf '%P\n'`; do
+    # echo $file
+    hashA=`md5sum $PATH_A/$file | awk '{print $1}'`
+    hashB=`md5sum $PATH_B/$file | awk '{print $1}'`
+    echo "[*] $file"
+    if test $hashA != $hashB; then
+      echo "    <>"
+    else
+      echo "    =="
+    fi
+  done
+fi
+
+# EOF
+
